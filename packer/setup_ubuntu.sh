0153565845ce78319637155c6bcd258817a7cb70
Adrian Todorov <adrian.ttodorov@gmail.com>
2021-11-21 17:34:46 +0100

    add extra setup_ubuntu.sh

--- packer/setup_ubuntu.sh
+++ packer/setup_ubuntu.sh
@@ -14,6 +14,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 apt-cache policy docker-ce
+sudo apt-get update 
 
 sudo apt-get install -y docker-ce
 sudo usermod -a -G docker ubuntu
